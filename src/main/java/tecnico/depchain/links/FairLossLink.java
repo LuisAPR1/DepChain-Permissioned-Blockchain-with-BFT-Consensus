58e73951b387c6ab3d5311163131f1e0873a0167
didas72 <diogocruzdiniz@gmail.com>
2026-03-01 17:56:24 +0000

    feat: Base class and FairLossLink

---
+++ src/main/java/tecnico/depchain/links/FairLossLink.java
@@ -0,0 +1,53 @@
+package tecnico.depchain.links;
+
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.net.DatagramSocket;
+import java.io.IOException;
+import java.net.DatagramPacket;
+
+public class FairLossLink extends P2PLink implements Runnable {
+	private DatagramSocket sock;
+	private Thread receiverThread;
+
+	public FairLossLink(InetSocketAddress remote) throws SocketException {
+		super(remote);
+
+		sock = new DatagramSocket(remote); // FIXME: Never closed
+
+		// Start receiver thread
+		receiverThread = new Thread(this);
+		receiverThread.start();
+	}
+
+	@Override
+	public void Transmit(byte[] data) {
+		var packet = new DatagramPacket(data, data.length, remote);
+		try {
+			sock.send(packet);
+		}
+		catch (IOException e) {
+			// Ignore
+		}
+	}
+
+	// Receiver thread
+	public void run()
+	{
+		byte[] rxBuffer = new byte[64 * 1024];
+		DatagramPacket packet = new DatagramPacket(rxBuffer, rxBuffer.length);
+		while (true)
+		{
+			try {
+				sock.receive(packet);
+			}
+			catch (IOException e) {
+				// Ignore
+				continue;
+			}
+
+			if (rxHandler != null)
+				rxHandler.accept(rxBuffer, this);
+		}
+	}
+}
---
+++ src/main/java/tecnico/depchain/links/P2PLink.java
@@ -0,0 +1,19 @@
+package tecnico.depchain.links;
+
+import java.net.InetSocketAddress;
+import java.util.function.BiConsumer;
+
+public abstract class P2PLink {
+	public InetSocketAddress remote;
+
+	public P2PLink(InetSocketAddress remote)
+	{
+		assert remote != null;
+		this.remote = remote;
+	}
+
+	public InetSocketAddress getRemote() { return remote; }
+
+	public BiConsumer<byte[], P2PLink> rxHandler = null;
+	public abstract void Transmit(byte[] data);
+}
